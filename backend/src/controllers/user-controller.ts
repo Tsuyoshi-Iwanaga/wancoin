9ad887b9c476750f91c12ed58f78842f6ab252cc
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2023-01-09 20:33:43 +0900

    add user creation api

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -6,13 +6,15 @@ import {
 } from 'iroha-helpers/lib/proto/endpoint_grpc_pb'
 import commands from 'iroha-helpers/lib/commands'
 import queries from 'iroha-helpers/lib/queries'
+import keyCreate from '../helper/keycreate'
+
+const util = require('util')
 
 //load env
 require('dotenv').config()
-
-const adminId = process.env.ADMIN_ID
-const adminPriv = process.env.ADMIN_PRIVATE_KEY
-const irohaEndpoint = process.env.IROHA_ADDRESS
+const adminId = process.env.ADMIN_ID || ''
+const adminPriv = process.env.ADMIN_PRIVATE_KEY || ''
+const irohaEndpoint = process.env.IROHA_ADDRESS || ''
 
 //client
 const queryService = new QueryService_v1Client(
@@ -23,8 +25,23 @@ const commandService = new CommandService_v1Client(
   irohaEndpoint ? irohaEndpoint : '',
   grpc.credentials.createInsecure()
 )
+//コマンド実行後に完了したブロックを収集
+queries.fetchCommits({
+    privateKey: adminPriv,
+    creatorAccountId: adminId,
+    queryService,
+    timeoutLimit: 4000
+  },
+  (block) => {
+    console.log('fetchCommits new block:', util.inspect(block, false, null))
+  },
+  (error) => {
+    throw new Error(`fetchCommits failed : ${error.stack}`)
+  }
+)
 
 const userController = {
+  //ユーザ情報を得る
   getUser: (req: express.Request, res: express.Response) => {
     try {
       Promise.all([
@@ -59,14 +76,41 @@ const userController = {
       })
     }
   },
+  //ユーザを作成する
   createUser: (req: express.Request, res: express.Response) => {
     try {
-      res.status(200).json({
-        message: "登録完了"
+      const { account, domain, pub, priv } = keyCreate(req.body.domain, req.body.account)
+
+      if(!(account && domain && pub && priv)) {
+        throw new Error('keypair creation failed..')
+      }
+
+      Promise.all([
+        commands.createAccount({
+          privateKeys: [adminPriv],
+          creatorAccountId: adminId,
+          quorum: 1,
+          commandService,
+          timeoutLimit: 5000
+        }, {
+          accountName: account ,
+          domainId: domain,
+          publicKey: pub
+        })
+      ])
+      .then(data => {
+        res.status(200).json(Object.assign(data, { account, domain, pub, priv }))
       })
+      .catch(err => {
+        throw new Error(`${err.message}: account creation failed..`)
+      })
+      .finally(() => {
+        process.exit(0)
+      })
+
     } catch (err) {
       res.status(400).json({
-        message: 'error'
+        message: err ? err : 'Error!!'
       })
     }
   }
---
+++ backend/src/helper/keycreate.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs'
+
+export default function keyCreate(domain: string, account: string) {
+
+  if(!(domain && account)) {
+    throw new Error('入力された内容が不正です')
+  }
+
+  let public_key = ''
+  let private_key = ''
+  const KEY_DIR = './keys/'
+
+  let ed25519 = require('ed25519.js')
+  let keys = ed25519.createKeyPair()
+  let pub = keys.publicKey
+  let priv = keys.privateKey
+
+  for (var i = 0; i < 32; i++) {
+    public_key = public_key + pub[i].toString(16).padStart(2, '0')
+  }
+  for (var i = 0; i < 32; i++) {
+    private_key = private_key + priv[i].toString(16).padStart(2, '0')
+  }
+
+  fs.writeFile(KEY_DIR + account + '@' + domain + '.pub', public_key , function (err:any) {
+    if (err) { console.log(err)
+      throw err }
+  })
+  fs.writeFile(KEY_DIR + account + '@' + domain + '.priv', private_key , function (err:any) {
+    if (err) { console.log(err)
+      throw err }
+  })
+
+  return {
+    account: account,
+    domain: domain,
+    pub: public_key,
+    priv: private_key
+  }
+}
