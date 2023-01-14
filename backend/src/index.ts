801f7dfd3d33aaf84960bcb3af3ded0f7a9c9c46
Tsuyoshi Iwanaga <tsuyo.1991@gmail.com>
2023-01-15 02:34:28 +0900

    setting cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,10 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+const cors = require('cors')
+app.use(cors())
+
 app.use('/', router)
 
 const server = https.createServer({
